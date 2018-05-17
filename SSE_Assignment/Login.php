2a25446cccf99f7b8021dcbea81e49a1df535cbe
AnuCS <39376934+AnuCS@users.noreply.github.com>
2018-05-17 21:11:03 +0530

    Add files via upload

---
+++ SSE_Assignment/Login.php
@@ -0,0 +1,16 @@
+<?php
+require  'testses.php';
+?>
+<html>
+	<head>
+		<title>Login</title>
+	</head>
+	<body>
+		<form action="validation.php" method="post">
+			user<input type="text" name="user" />
+			password<input type="text" name="pas"/>
+			<input type="submit" value="enter"/>
+			<input type="hidden" name="token" value="<?php echo $_SESSION['CSRF_token']; ?>">
+		</form>
+	</body>
+</html>
---
+++ SSE_Assignment/validation.php
@@ -0,0 +1,29 @@
+<html>
+<?php
+require  'testses.php';
+
+ $pas = $_REQUEST['pas'];
+ 
+?>
+
+
+	<head>
+		<title>validation</title>
+	</head>
+	<body>
+		<?php
+		if($pas == "123"){
+			?>
+			Valida user
+			<?php
+		}
+		else {
+			?>
+			Invalid user
+			<?php
+		}
+		?>
+		
+		<p><?php print($_SESSION['CSRF_token']) ?></p>
+	</body>
+</html>
